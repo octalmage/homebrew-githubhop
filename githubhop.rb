5d67d7de6fa24168603958ca3168be8678287cba
goreleaserbot <goreleaser@carlosbecker.com>
2018-05-28 17:18:31 -0500

    Brew formula update for githubhop version v0.0.4

--- githubhop.rb
+++ githubhop.rb
@@ -1,9 +1,9 @@
 class Githubhop < Formula
   desc ""
   homepage ""
-  url "https://github.com/octalmage/githubhop/releases/download/v0.0.3/githubhop_0.0.3_darwin_amd64.tar.gz"
-  version "0.0.3"
-  sha256 "e9860793247f4c0515bd4498747b8d07434897d5e8870944ba7f6b4597fcc561"
+  url "https://github.com/octalmage/githubhop/releases/download/v0.0.4/githubhop_0.0.4_darwin_amd64.tar.gz"
+  version "0.0.4"
+  sha256 "aacd4dc76e54d1fa133af9b0a4610794ef301d4db4f1207c918cc571ca1dd4f9"
 
   def install
     bin.install "githubhop"
